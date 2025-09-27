354c83f1ef41bba004f0c00d448b6a689bc9cea5
kaledmhd1 <Isameryem211@gmail.com>
2025-09-27 23:15:22 +0100

    Update and rename app (5).py to app.py

--- app.py
+++ app.py
@@ -5240,7 +5240,7 @@ group_accounts = [
   }
 ]
 
-JWT_API_TEMPLATE = "https://jwt-three-weld.vercel.app/api/oauth_guest?uid={uid}&password={password}"
+JWT_API_TEMPLATE = "https://jwt-lac.vercel.app/api/oauth_guest?uid={uid}&password={password}"
 
 CACHE = {
     "tokens": {},   # dict {uid: token}
